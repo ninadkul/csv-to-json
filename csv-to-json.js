2bec574553a4d2614fca928742f48e5899751492
ninadkul <41394535+ninadkul@users.noreply.github.com>
2018-07-18 15:30:37 -0500

    Update csv-to-json.js
    cleanup

--- csv-to-json.js
+++ csv-to-json.js
@@ -2,22 +2,18 @@ const fs = require('fs')
 const path = require('path')
 const csvtojson = require('csvtojson')
 const csvFile = 'customer-data.csv'
-var jsonString = ''
+
 
 csvtojson().fromFile(csvFile).then((jsonObj)=> {
+    
     let str = ''
-    try{
     str = JSON.stringify(jsonObj)
-}catch(e){
-    console.log(e)
-}
-jsonString += str
-//console.log(jsonString)
-fs.writeFileSync(path.join(__dirname,'customer-data.json'),str)
+    console.log(str)
+    fs.writeFileSync(path.join(__dirname,'customer-data.json'),str)
 
 })
-//console.log(`${jsonString}`)
-const jsonArray= csvtojson().fromFile(csvFile);
+
+csvtojson().fromFile(csvFile);
 
 
 
